7cdeb5ec740eef7130acd097aafd57a04c4e3eba
dori22 <dorin.bochis@gmail.com>
2018-05-24 20:22:48 +0300

    Code for finalquiz

---
+++ finalquiz/src/main/java/Package.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+
+public class Package implements Comparable<Package>{
+
+    private String targetLocation;
+    private double distance;
+    private double merchandiseValue;
+    private Date deliveryDate;
+
+    public Package(String targetLocation, double distance, double merchandiseValue, Date deliveryDate) {
+        this.targetLocation = targetLocation;
+        this.distance = distance;
+        this.merchandiseValue = merchandiseValue;
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getTargetLocation() {
+        return targetLocation;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getMerchandiseValue() {
+        return merchandiseValue;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setTargetLocation(String targetLocation) {
+        this.targetLocation = targetLocation;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void setMerchandiseValue(double merchandiseValue) {
+        this.merchandiseValue = merchandiseValue;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public int compareTo(Package o) {
+
+        if()
+
+
+    }
+}
+
+
+
